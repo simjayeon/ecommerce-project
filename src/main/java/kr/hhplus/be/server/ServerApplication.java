9bf405266a4426c7d9aaafc909fe3abfde689f70
nature <simjayeon98@gmail.com>
2025-03-28 16:56:04 +0900

    [DEV] @EnableJpaRepositories 수정

--- src/main/java/kr/hhplus/be/server/ServerApplication.java
+++ src/main/java/kr/hhplus/be/server/ServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "kr.hhplus.be.server.infra.jpa")
+@EnableJpaRepositories(basePackages = "kr.hhplus.be.server.infra.repository")
 public class ServerApplication {
 
 	public static void main(String[] args) {
